11e162ff059b3ac5382ed80cffc0e58a15c729a4
TsaiRongFu <q0975040879@gmail.com>
2021-05-22 23:24:13 +0800

    Line-Notify

---
+++ Line-Notify.py
@@ -0,0 +1,16 @@
+import requests
+
+def lineNotifyMessage(token, msg):
+    headers = {
+        "Authorization": "Bearer " + token, 
+    }
+    payload = {'message': msg}
+    r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+    return r.status_code
+    
+# 修改為你要傳送的訊息內容
+message = '\ntest \n\n••••••••••••••••••••••••••••••••••••••\n 123456'
+# 修改為你的權杖內容
+token = ''
+
+lineNotifyMessage(token, message)
